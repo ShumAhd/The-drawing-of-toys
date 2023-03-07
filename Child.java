4d32d1ca51ea03d7ec5b830f755113e302b634bf
ShuMAhd <shuman@zykaika.ru>
2023-03-07 21:29:11 +0800

    Заполнил класс Child кодом

--- Child.java
+++ Child.java
@@ -1,5 +1,34 @@
 
 
 public class Child {
+  protected String name;
+  protected String prize;
+
+  @SuppressWarnings("unused")
+  public Child() {
+    this.name = "";
+    this.prize = null;
+  }
+
+  public Child(String name) {
+    this.name = name;
+    this.prize = null;
+  }
+
+  // геттеры - сеттеры
+  public String getName() { return this.name; }
+  public String getPrize() { return (this.prize != null) ? this.prize : ""; }
+
+  @SuppressWarnings("unused")
+  public void setName(String name) { this.name = name; }
+  public void setPrize(String prize) { this.prize = prize; }
+
+  @Override
+  public String toString() {
+    return String.format("Name: %s, Prize: %s"
+        , getName()
+        , getPrize()
+    );
+  }
 
 }
